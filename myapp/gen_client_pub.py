3b5b49749cc2722e13d525ffc6f0c48b90f13599
hehehe <23520928@gm.uit.edu.vn>
2025-06-08 21:16:52 +0700

    delete music folder and scripts

--- myapp/gen_client_pub.py
+++
@@ -1,6 +0,0 @@
-# scripts/gen_client_pub.py
-from crypto_utils import generate_ecdhe_keypair, serialize_public_key
-
-priv, pub = generate_ecdhe_keypair()
-client_pub_hex = serialize_public_key(pub).hex()
-print(client_pub_hex)
